c7f38c1f217b399b04bbdc335d5cbaaf45506fc0
Rockbesst Win <rockbesst@gmail.com>
2021-05-30 15:24:47 +0300

    add rule to gitignore

--- ready_webserver.sh
+++ ready_webserver.sh
@@ -2,6 +2,6 @@
 yum -y update
 yum -y install httpd
 ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>Server IP is $ip.</h2>" > /var/www/html/index.html
+echo "<h2>Server IP is $ip</h2>" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
