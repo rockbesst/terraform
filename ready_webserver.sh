811acac2a1506c68ded0f15965500decbc579207
Rockbesst Win <rockbesst@gmail.com>
2021-05-31 18:04:26 +0300

    1

--- ready_webserver.sh
+++ ready_webserver.sh
@@ -2,7 +2,8 @@
 yum -y update
 yum -y install httpd
 ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+aws s3 cp s3://rockbesst-img/IMG_3993.JPG IMG_3993.JPG
 echo "<h2>Server IP is $ip</h2>" > /var/www/html/index.html
-echo "<br><img src = 'https://rockbesst-img.s3.eu-central-1.amazonaws.com/IMG_3993.JPG'>"
+echo "<br><img src = 'IMG_3993.JPG'>"
 sudo service httpd start
 chkconfig httpd on
