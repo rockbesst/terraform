fe3a663811d24232c77165480efca84802115699
Rockbesst Win <rockbesst@gmail.com>
2021-05-31 18:37:31 +0300

    added resolution for photo

--- ready_webserver.sh
+++ ready_webserver.sh
@@ -4,6 +4,6 @@ yum -y install httpd
 ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 aws s3 cp s3://rockbesst-img/IMG_3993.JPG /var/www/html/IMG_3993.JPG
 echo "<h2>Server IP is $ip</h2>" > /var/www/html/index.html
-echo "<br><img src = 'IMG_3993.JPG'>" > /var/www/html/index.html
+echo "<br><img src = 'IMG_3993.JPG' style="width:500px;height:600px;>" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
