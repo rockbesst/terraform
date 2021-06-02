223629ed8d2b2760c3bc65e79bdc2479ae2510c0
Rockbesst Win <rockbesst@gmail.com>
2021-06-02 16:33:32 +0300

    changes in security.tf

--- security.tf
+++ security.tf
@@ -4,15 +4,10 @@ data "aws_security_group" "mainSecGroup" {
 }
 # IAM ###################################################
 
- resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
-
+ 
 resource "aws_iam_role" "ec2_role" {
   name = "ec2_role"
   path = "/"
-
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -29,10 +24,14 @@ resource "aws_iam_role" "ec2_role" {
 }
 EOF
 }
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
 
-resource "aws_iam_role_policy" "ec2_policy" {
+
+resource "aws_iam_policy" "ec2_policy" {
   name = "ec2_policy"
-  role = aws_iam_role.ec2_role.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -43,4 +42,10 @@ resource "aws_iam_role_policy" "ec2_policy" {
       },
     ]
   })
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      =  ["${aws_iam_role.ec2_role.name}"]
+  policy_arn = aws_iam_policy.ec2_policy.arn
 }
